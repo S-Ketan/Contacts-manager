7c9ba6641f4e49da8a5c5c53c6adb6f38f38e7ba
S-Ketan <ketan10923@gmail.com>
2024-12-21 03:23:48 +0100

    Add bcrypt for password hashing and update user registration logic

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,6 +1,31 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../Models/UserModel");
+const bcrypt = require("bcrypt");
+
 
 const userRegister = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("Please provide all fields");
+    }
+
+    const userAvailable = await User.findOne({email});
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    console.log(hashedPassword);
+
+        const user = await User.create({username, email, password:hashedPassword});
+        if (!user) {
+            res.status(400);
+            throw new Error("Invalid user data");
+        }
+        res.status(201).json({_id : user._id, username: user.username, email: user.email});
+        console.log(user);
+    
 res.json({message: "Register user"});
 });
 const userLogin = asyncHandler(async (req, res) => {
--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
  const userSchema = mongoose.Schema({
-        name:{
+        username:{
             type:String,
             required:[true,'Please provide a name']
         },
