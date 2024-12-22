4d2f3aa66c8dcf6f9e8067d6b0043e8e87aee4d1
S-Ketan <ketan10923@gmail.com>
2024-12-22 03:35:15 +0100

    Restrict contact access to authenticated users and associate contacts with user IDs

--- Controllers/ContactController.js
+++ Controllers/ContactController.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 const Contact = require("../Models/ContactModel");
 
 const getContacts = asyncHandler(async (req, res) => {
-  const contacts = await Contact.find({});
+  const contacts = await Contact.find({user_id: req.user.id});
   res.status(200).json(contacts);
 });
 
@@ -12,7 +12,7 @@ const createContact = asyncHandler(async (req, res) => {
   if (!name || !email || !phone) {
     res.status(400).json({message : "Please provide name, email and phone"});
   }
-  const contact = await Contact.create({ name, email, phone });
+  const contact = await Contact.create({ name, email, phone, user_id: req.user.id });
   res.status(201).json(contact);
 });
 
@@ -27,6 +27,10 @@ const updateContact = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
   if (!contact) {
     res.status(404).json({message :"Contact not found"});
+  }
+
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(401).json({message : "Not authorized to update contact"});
   }
   const updatedContact = await Contact.findByIdAndUpdate(
     req.params.id,
@@ -43,7 +47,10 @@ const deleteContact = asyncHandler(async (req, res) => {
   if (!contact) {
     res.status(404).json({message :"Contact not found"});
   }
-  await Contact.findByIdAndDelete(req.params.id);
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(401).json({message : "Not authorized to delete contact"});
+  }
+  await Contact.deleteOne({_id : req.params.id});
   res.status(200).json({ message: "Contact removed" });
 });
 
